68e8ed6ebf9f33bdbbad5b1d92baca03ae2ffb32
Gabriel Wilkinson <gabriel.wilkinson@hotmail.com>
2023-01-11 12:54:29 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {title: 'Car Insurance',
+     amount: 294.67}
+  ]
+
   return (
     <div>
       <h2>Let's get started!</h2>
       <pe>This is also visible!</pe>
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
       <ExpenseItem></ExpenseItem>
       <ExpenseItem></ExpenseItem>
     </div>
